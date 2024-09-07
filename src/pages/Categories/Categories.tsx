945a8a4be7e856b6185043bc1588bd4ff676040c
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2024-09-07 16:26:19 +0530

    category save api created

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,8 +1,35 @@
 import {TextField} from "../../component/TextField/TextFild";
 import {TextArea} from "../../component/TextArea/TextArea";
 import {Button} from "../../component/Button/Button";
+import React, {useState} from "react";
+import api from "./api";
 
 export const Categories = () => {
+    const [categoryData, setCategoryData] = useState({
+        name: '',
+        description: '',
+    });
+    type CategoryDataKey = keyof typeof categoryData;
+
+    const handleCategoryChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const {name, value} = e.target;
+        const typedName = name as CategoryDataKey;
+
+        setCategoryData({
+            ...categoryData,
+            [typedName]: value,
+        });
+    };
+
+    const handleCategorySaveEvent = async () => {
+        const isSuccess = await api.saveCategory(categoryData);
+        if (isSuccess) {
+            alert("Data saved successfully!");
+        } else {
+            alert("Failed to save data.");
+        }
+    };
+
     return (
         <section className='h-max flex w-[95%] flex-col justify-center'>
             <section className='flex flex-row justify-start mt-5'>
@@ -11,13 +38,30 @@ export const Categories = () => {
             {/*url display section*/}
             <section className='flex flex-row flex-wrap items-center justify-center mt-5 p-5 rounded-xl shadow-md'>
                 <div className='flex flex-row flex-wrap items-center justify-center w-full'>
-                    <TextField placeholder={'Category name'} label={'Category name'} important={"*"}/>
+                    <TextField
+                        name="name"
+                        placeholder={'Category name'}
+                        label={'Category name'}
+                        important={"*"}
+                        value={categoryData.name}
+                        onChange={handleCategoryChange}
+                    />
                 </div>
                 <div className='flex flex-row flex-wrap items-center justify-center w-full'>
-                    <TextArea placeholder={'Description'} label={'Description'}/>
+                    <TextArea
+                        name="description"
+                        placeholder={'Description'}
+                        label={'Description'}
+                        value={categoryData.description}
+                        onChange={handleCategoryChange}
+                    />
                 </div>
                 <div className='flex flex-row flex-wrap items-center justify-end w-full'>
-                    <Button name={'Save'} color={'bg-[#2FEB00]'}/>
+                    <Button
+                        name={'Save'}
+                        color={'bg-[#2FEB00]'}
+                        onClick={handleCategorySaveEvent}
+                    />
                 </div>
             </section>
         </section>
