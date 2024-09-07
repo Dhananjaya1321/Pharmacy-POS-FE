1f00e3a2ff0757277cab89be20aa0893370d6cf8
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2024-09-07 19:24:07 +0530

    unit save api created

--- src/pages/Units/Units.tsx
+++ src/pages/Units/Units.tsx
@@ -1,8 +1,37 @@
 import {TextField} from "../../component/TextField/TextFild";
 import {TextArea} from "../../component/TextArea/TextArea";
 import {Button} from "../../component/Button/Button";
+import React, {useState} from "react";
+import api from "./api";
 
 export const Units = () => {
+    const [unitData, setUnitData] = useState({
+        unitName: '',
+        unitSymbology: '',
+        description: '',
+    });
+    type UnitDataKey = keyof typeof unitData;
+
+    const handleUnitChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const {name, value} = e.target;
+        const typedName = name as UnitDataKey;
+
+        setUnitData({
+            ...unitData,
+            [typedName]: value,
+        });
+    };
+
+    const handleUnitSaveEvent = async () => {
+        const isSuccess = await api.saveUnit(
+            unitData);
+        if (isSuccess) {
+            alert("Data saved successfully!");
+        } else {
+            alert("Failed to save data.");
+        }
+    };
+
     return (
         <section className='h-max flex w-[95%] flex-col justify-center'>
             <section className='flex flex-row justify-start mt-5'>
@@ -11,14 +40,38 @@ export const Units = () => {
             {/*url display section*/}
             <section className='flex flex-row flex-wrap items-center justify-center mt-5 p-5 rounded-xl shadow-md'>
                 <div className='flex flex-row flex-wrap items-center justify-center w-full'>
-                    <TextField placeholder={'Kilogram'} label={'Unit name'} important={"*"}/>
-                    <TextField placeholder={'kg'} label={'Unit symbology'} important={"*"}/>
+                    <TextField
+                        name="unitName"
+                        placeholder={'Kilogram'}
+                        label={'Unit name'}
+                        important={"*"}
+                        value={unitData.unitName}
+                        onChange={handleUnitChange}
+                    />
+                    <TextField
+                        name="unitSymbology"
+                        placeholder={'kg'}
+                        label={'Unit symbology'}
+                        important={"*"}
+                        value={unitData.unitSymbology}
+                        onChange={handleUnitChange}
+                    />
                 </div>
                 <div className='flex flex-row flex-wrap items-center justify-center w-full'>
-                    <TextArea placeholder={'Description'} label={'Description'}/>
+                    <TextArea
+                        name="description"
+                        placeholder={'Description'}
+                        label={'Description'}
+                        value={unitData.description}
+                        onChange={handleUnitChange}
+                    />
                 </div>
                 <div className='flex flex-row flex-wrap items-center justify-end w-full'>
-                    <Button name={'Save'} color={'bg-[#2FEB00]'}/>
+                    <Button
+                        name={'Save'}
+                        color={'bg-[#2FEB00]'}
+                        onClick={handleUnitSaveEvent}
+                    />
                 </div>
             </section>
         </section>
