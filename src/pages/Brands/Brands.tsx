5c3df5448825390fc76bdb85fb51f1d58206e280
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2024-09-07 22:11:56 +0530

    brand save api created

--- src/pages/Brands/Brands.tsx
+++ src/pages/Brands/Brands.tsx
@@ -1,8 +1,39 @@
 import {TextField} from "../../component/TextField/TextFild";
 import {TextArea} from "../../component/TextArea/TextArea";
 import {Button} from "../../component/Button/Button";
+import React, {useState} from "react";
+import api from "./api";
 
 export const Brands = () => {
+    const [brandData, setBrandData] = useState({
+        name: '',
+        contact: '',
+        website: '',
+        address: '',
+        description: '',
+    });
+    type BrandDataKey = keyof typeof brandData;
+
+    const handleBrandChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const {name, value} = e.target;
+        const typedName = name as BrandDataKey;
+
+        setBrandData({
+            ...brandData,
+            [typedName]: value,
+        });
+    };
+
+    const handleBrandSaveEvent = async () => {
+        const isSuccess = await api.saveUnit(
+            brandData);
+        if (isSuccess) {
+            alert("Data saved successfully!");
+        } else {
+            alert("Failed to save data.");
+        }
+    };
+
     return (
         <section className='h-max flex w-[95%] flex-col justify-center'>
             <section className='flex flex-row justify-start mt-5'>
@@ -11,16 +42,50 @@ export const Brands = () => {
             {/*url display section*/}
             <section className='flex flex-row flex-wrap items-center justify-center mt-5 p-5 rounded-xl shadow-md'>
                 <div className='flex flex-row flex-wrap items-center justify-center w-full'>
-                    <TextField placeholder={'Brand name'} label={'Brand name'} important={"*"}/>
-                    <TextField placeholder={'077 752 0000'} label={'Contact'} important={"*"}/>
-                    <TextField placeholder={'brand.com'} label={'Website'}/>
+                    <TextField
+                        name="name"
+                        placeholder={'Brand name'}
+                        label={'Brand name'} important={"*"}
+                        value={brandData.name}
+                        onChange={handleBrandChange}
+                    />
+                    <TextField
+                        name="contact"
+                        placeholder={'077 752 0000'}
+                        label={'Contact'} important={"*"}
+                        value={brandData.contact}
+                        onChange={handleBrandChange}
+                    />
+                    <TextField
+                        name="website"
+                        placeholder={'brand.com'}
+                        label={'Website'}
+                        value={brandData.website}
+                        onChange={handleBrandChange}
+                    />
                 </div>
                 <div className='flex flex-row flex-wrap items-center justify-center w-full'>
-                    <TextArea placeholder={'Address'} label={'Address'}/>
-                    <TextArea placeholder={'Description'} label={'Description'}/>
+                    <TextArea
+                        name="address"
+                        placeholder={'Address'}
+                        label={'Address'}
+                        value={brandData.address}
+                        onChange={handleBrandChange}
+                    />
+                    <TextArea
+                        name="description"
+                        placeholder={'Description'}
+                        label={'Description'}
+                        value={brandData.description}
+                        onChange={handleBrandChange}
+                    />
                 </div>
                 <div className='flex flex-row flex-wrap items-center justify-end w-full'>
-                    <Button name={'Save'} color={'bg-[#2FEB00]'}/>
+                    <Button
+                        name={'Save'}
+                        color={'bg-[#2FEB00]'}
+                        onClick={handleBrandSaveEvent}
+                    />
                 </div>
             </section>
         </section>
---
+++ src/pages/Brands/api.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import {base_url} from "../../config/apiConfig";
+
+const api = {
+    saveUnit: async (brand: any) => {
+        try {
+            const response = await axios.post(
+                `${base_url}/brand`,
+                brand
+            );
+            return response.status === 200;
+        } catch (error) {
+            console.error("There was an error saving the data!", error);
+            return false;
+        }
+    },
+};
+
+export default api;
